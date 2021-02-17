a754d8f603ced42dc4d76f6cde4fe20157f9a0c2
Charles Butterworth <charlessbutterworth@gmail.com>
2021-02-17 11:55:21 -0600

    finsihed ch up to ch12

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,13 +1,42 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { GameList } from "./game/GameList.js"
+import { GameProvider } from "./game/GameProvider.js"
+import {EventList} from "./event/EventList"
+import {EventProvider} from "./event/EventProvider"
+import {GameForm} from "./game/GameForm"
+import {EventForm} from "./event/EventForm"
 
 export const ApplicationViews = () => {
     return <>
         <main style={{
             margin: "5rem 2rem",
-            backgroundColor: "lightgoldenrodyellow"
+            lineHeight: "1.75rem"
         }}>
-            Application views
+            <GameProvider>
+                <Route exact path="/games">
+                    <GameList />
+                </Route>
+                <Route exact path="/games/new">
+                <GameForm />
+            </Route>
+            </GameProvider>
+
+            <EventProvider>
+                <Route exact path="/events">
+                    <EventList />
+                </Route>
+            </EventProvider>
+
+        <GameProvider>
+            <EventProvider>
+                <Route exact path="/events/new">
+                    <EventForm />
+                </Route>
+            </EventProvider>
+        </GameProvider>
+        
+
         </main>
     </>
 }
